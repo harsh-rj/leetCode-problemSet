a6a2d42c6ab542ded522a9a092b8edb6c5f489a9
Harsh <harshraj6731@gmail.com>
2025-11-09 11:27:21 +0530

    not working code, best method yet but not working

--- p189-rotateArray/rotateArray.cpp
+++ p189-rotateArray/rotateArray.cpp
@@ -1,4 +1,4 @@
-//will not work if n-k < k :(
+//will not work if n-k < k :( --> i have added two cases but still doesn't work
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -17,34 +17,54 @@ void rotate(vector<int> &nums, int k)
     }
     else
     {
-        if (n % k == 0)
-            endMove = 0;
-        j = n - k;
-        while (true)
-        {
-            i = j - k;
-            for (int index = 0; index < k; index++)
+        if (n % k == 0) endMove = 0;
+        if (n-k > k) {
+            j = n - k;
+            while (true)
             {
-                swap(nums.at(i + index), nums.at(j + index));
+                i = j - k;
+                for (int index = 0; index < k; index++)
+                {
+                    swap(nums.at(i + index), nums.at(j + index));
+                }
+                j -= k;
+                if (j == 0)
+                    break;
+                if ((j == 1) && (k != 1))
+                    break;
             }
-            j -= k;
-            if (j == 0)
-                break;
-            if ((j == 1) && (k != 1))
-                break;
+            if (endMove)
+            {
+                for (int index = 0; index < k; index++)
+                {
+                    swap(nums.at(index), nums.at(index + 1));
+                }
+            }
         }
-        if (endMove)
-        {
-            for (int index = 0; index < k; index++)
+        else {
+            k = n-k;
+            j = 0;
+            while (true)
             {
-                swap(nums.at(index), nums.at(index + 1));
+                i = j + k;
+                for (int index = 0; index < k; index++)
+                {
+                    swap(nums.at(i + index), nums.at(j + index));
+                }
+                j += k;
+                if (j == (n-k))
+                    break;
+                if ((j == (n-k-1)) && (k != 1))
+                    break;
             }
+            if (endMove)
+            {
+                for (int index = 0; index < k; index++)
+                {
+                    swap(nums.at(n - 1 -index), nums.at(n - 1 - index - 1));
+                }
+            }
+
         }
     }
-}
-
-int main()
-{
-
-    return 0;
 }
